88ee21de564bdf8743b198fe7cac75e1691d1994
SwarupVishwas18 <swarupvishwas000@gmail.com>
2023-06-30 18:01:37 +0530

    Iron Man Facts

---
+++ marvel-fun-facts/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './comp/Navbar'
+import FunFacts from './comp/FunFacts'
+import './App.css'
+
+function App() {
+ 
+  
+  return (
+    <div className='app'>
+      <Navbar />
+      <FunFacts />
+    </div>
+  )
+}
+
+export default App
---
+++ marvel-fun-facts/src/comp/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return (
+        <nav className="nav">
+            <h1 id="logo">IRON MAN</h1>
+            <div className="git-link"><a href="">More Mini Project</a></div>
+        </nav>
+    )
+}
