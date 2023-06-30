ab178fcde961329e02f953b4ed9280f7c11341a3
Swarup Vishwas <swarupvishwas000@gmail.com>
2023-07-01 00:28:48 +0530

    Update Navbar.jsx

--- marvel-fun-facts/src/comp/Navbar.jsx
+++ marvel-fun-facts/src/comp/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar(){
     return (
         <nav className="nav">
             <h1 id="logo">IRON MAN</h1>
-            <div className="git-link"><a href="">More Mini Project</a></div>
+            <div className="git-link"><a href="https://www.github.com/SwarupVishwas18/reac">More Mini Project</a></div>
         </nav>
     )
 }
