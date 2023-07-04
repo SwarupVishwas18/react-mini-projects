28434e2b9817c959730dbb04f96d2a17c358de9f
Swarup Vishwas <swarupvishwas000@gmail.com>
2023-07-04 15:54:38 +0530

    Update Navbar

--- manga-searcher/src/comp/Navbar.jsx
+++ manga-searcher/src/comp/Navbar.jsx
@@ -1,14 +1,15 @@
-export default function Navbar(){
-    return (
-        <nav className="nav">
-            <h1 className="logo">
-                MANGAPEDIA
-            </h1>
-            <div className="pc-link">
-                <a href="">
-                    Repo Link
-                </a>
-            </div>
-        </nav>
-    )
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <h1 className="logo">MANGAPEDIA</h1>
+      <div className="pc-link">
+        <a
+          href="github.com/SwarupVishwas18/react-mini-projects"
+          target="_blank"
+        >
+          Repo Link
+        </a>
+      </div>
+    </nav>
+  );
 }
